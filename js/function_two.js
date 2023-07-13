fdba62caf35198f087316a0b9f7de84a15555d57
Ilya <ilieturcan424@gmail.com>
2023-07-13 20:10:02 +0300

    asdddddddddd

--- js/function_two.js
+++ js/function_two.js
@@ -2,42 +2,42 @@
 "use strict";
 
 function sayHello(a) {
-	console.log(`Hello, ${a}`);
-	return `Hello, ${a}`;
+    console.log(`Hello, ${a}`);
+    return `Hello, ${a}`;
 	
 }
 
 sayHello("Anton");
 
 function returnNeighboringNumbers(a) {
-	const b = [a - 1, a, a + 1];
-	console.log(b);
-	return b;
+    const b = [a - 1, a, a + 1];
+    console.log(b);
+    return b;
     
 }
 
 returnNeighboringNumbers(5);
 
 function getMathResult(a, b) {
-	let sum = 0;
+    let sum = 0;
 	
-	if (typeof (b) === "string") {
-		return a;
-	} else if (b <= 0) {
-		return a;
-	} else {
-		let sentence = "";
-		for (let i = 1; i <= b; i++) {
-			sum += a;
-			if (i == b) {
-				sentence += `${sum}`;
-			} else {
-				sentence += `${sum}---`;
-			}
-		}
-		console.log(sentence);
-		return sentence;
-	}
+    if (typeof (b) === "string") {
+        return a;
+    } else if (b <= 0) {
+        return a;
+    } else {
+        let sentence = "";
+        for (let i = 1; i <= b; i++) {
+            sum += a;
+            if (i == b) {
+                sentence += `${sum}`;
+            } else {
+                sentence += `${sum}---`;
+            }
+        }
+        console.log(sentence);
+        return sentence;
+    }
 }
 
 getMathResult(10, 10);
--- js/homework_3.js
+++ js/homework_3.js
@@ -1,3 +1,41 @@
 function getTimeFromMinutes(number) { 
-    
+
+    while (isNaN(number) || number < 0 || !Number.isInteger(number)) {
+        return "Ошибка, проверьте данные";
+    }
+
+    let minutes = 0;
+    let hours = 0;
+    hours = Math.floor(number / 60);
+    minutes = number % 60;
+
+
+    let hoursStr = "";
+
+    switch (hours) {
+    case 1: 
+        hoursStr = "час";
+        break;
+    case 2:
+    case 3:
+    case 4:
+        hoursStr = "часа";
+        break;
+    default:
+        hoursStr = "часов";
+        break;	
+		
+    }
+    return `Это ${hours} ${hoursStr} и ${minutes} минут`;
 }
+
+getTimeFromMinutes(580);
+
+function findMaxNumber(number_one, number_two, number_three, number_four) {
+    while (isNaN(number_one) || isNaN(number_two) || isNaN(number_three) || isNaN(number_four)) {
+        return 0;
+    }
+    return Math.max(number_one, number_two, number_three, number_four);
+}
+
+findMaxNumber(1, 2 , 3, 4);
