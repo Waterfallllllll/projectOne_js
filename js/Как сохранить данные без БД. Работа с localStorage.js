26c74958f7baed855b7081ce0cd4221e10b589c6
Ilya <ilieturcan424@gmail.com>
2023-11-09 14:16:43 +0200

    Как сохранить данные без БД. Работа с localStorage

--- js/Как сохранить данные без БД. Работа с localStorage.js
+++ js/Как сохранить данные без БД. Работа с localStorage.js
@@ -23,7 +23,32 @@ if (localStorage.getItem("isChecked")) {
     checkbox.checked = true; 
 }
 
+if (localStorage.getItem("bg") === "changed") {
+    form.style.backgroundColor = "red";
+}
+
 checkbox.addEventListener("change", () => {
     localStorage.setItem("isChecked", true);
 });
 
+change.addEventListener("click", () => {
+    if (localStorage.getItem("bg") === "changed") {
+        localStorage.removeItem("bg");
+        form.style.backgroundColor = "#fff";
+    } else {
+        localStorage.setItem("bg", "changed");
+        form.style.backgroundColor = "red";
+    }
+});
+
+// Записываем объект в localStorage. Мы должны сделать stringify, так как в противном случае будед сохранен просто object, а не ключ и объект
+
+const persone = {
+    name: "Alex",
+    age: 25
+};
+
+const serializedPersone = JSON.stringify(persone);
+localStorage.setItem("alex", serializedPersone);
+
+console.log(JSON.parse(localStorage.getItem("alex")));
