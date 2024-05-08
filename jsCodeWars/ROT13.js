83f6b6d8031e20f21e35e2a59632d2aacd6c899b
Ilya <ilieturcan424@gmail.com>
2024-05-08 17:26:56 +0300

    Add ROT13 v0.0.2

--- jsCodeWars/ROT13.js
+++ jsCodeWars/ROT13.js
@@ -1,31 +1,50 @@
 "use strict";
 
 function rot13(str) {
-    const englishAlphabet = [];
+    const englishAlphabetLowerCase = [];
+    const englishAlphabetUpperCase = [];
+
     for (var i = 97; i <= 122; i++) {
-        englishAlphabet.push(String.fromCharCode(i));
+        englishAlphabetLowerCase.push(String.fromCharCode(i));
     }
-    console.log(englishAlphabet);
-    const arr = [...str.toLowerCase()];
+    
+    for (var j = 65; j <= 90; j++) {
+        englishAlphabetUpperCase.push(String.fromCharCode(j));
+    }
+
+    const arr = [...str];
     const secondArr = [];
     let sum = 0;
-    let count = 0;
-    console.log(arr);
+
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < englishAlphabet.length; j++) {
-            if (arr[i] == englishAlphabet[j]) {
-                if (englishAlphabet[j + 13] == undefined) {
-                    sum = j + 13;
-                    secondArr.push(englishAlphabet[sum - englishAlphabet.length]);
-                } else {
-                    secondArr.push(englishAlphabet[j + 13]);
+        for (let j = 0; j < englishAlphabetUpperCase.length; j++) {
+            if (arr[i] == arr[i].toUpperCase()) {
+                if (arr[i] == englishAlphabetUpperCase[j]) {
+                    if (englishAlphabetUpperCase[j + 13] == undefined) {
+                        sum = j + 13;
+                        secondArr.push(englishAlphabetUpperCase[sum - englishAlphabetUpperCase.length]);
+                    }
+                    else
+                    {
+                        secondArr.push(englishAlphabetUpperCase[j + 13]);
+                    }
+                } 
+            } else {
+                for (let j = 0; j < englishAlphabetLowerCase.length; j++) {
+                    if (arr[i] == englishAlphabetLowerCase[j]) {
+                        if (englishAlphabetLowerCase[j + 13] == undefined) {
+                            sum = j + 13;
+                            secondArr.push(englishAlphabetLowerCase[sum - englishAlphabetLowerCase.length]);
+                        } else {
+                            secondArr.push(englishAlphabetLowerCase[j + 13]);
+                        }
+                    } 
                 }
-            } 
+            }
         }
     }
-    console.log(count);
+    
     console.log(secondArr);
-    // console.log(arr);
 }
 
 rot13("EBG13 rknzcyr.");
