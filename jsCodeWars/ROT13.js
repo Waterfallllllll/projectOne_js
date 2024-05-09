7d05073fd0fa64b74045d5e0b9b604542f49e639
Ilya <ilieturcan424@gmail.com>
2024-05-09 18:01:54 +0300

    Add Rot13 v0.0.3

--- jsCodeWars/ROT13.js
+++ jsCodeWars/ROT13.js
@@ -15,10 +15,21 @@ function rot13(str) {
     const arr = [...str];
     const secondArr = [];
     let sum = 0;
+    let k = 0;
+
+
+    [...str].forEach(item => {
+        if (item == /\D/) {
+            console.log(item);
+        }
+    });
 
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < englishAlphabetUpperCase.length; j++) {
             if (arr[i] == arr[i].toUpperCase()) {
+                if (arr[i] == /\D/) {
+                    secondArr.push(arr[i]);
+                }
                 if (arr[i] == englishAlphabetUpperCase[j]) {
                     if (englishAlphabetUpperCase[j + 13] == undefined) {
                         sum = j + 13;
@@ -30,15 +41,20 @@ function rot13(str) {
                     }
                 } 
             } else {
-                for (let j = 0; j < englishAlphabetLowerCase.length; j++) {
-                    if (arr[i] == englishAlphabetLowerCase[j]) {
-                        if (englishAlphabetLowerCase[j + 13] == undefined) {
-                            sum = j + 13;
-                            secondArr.push(englishAlphabetLowerCase[sum - englishAlphabetLowerCase.length]);
-                        } else {
-                            secondArr.push(englishAlphabetLowerCase[j + 13]);
-                        }
-                    } 
+                if (k == arr.length) {
+                    break;
+                }
+                for (k = i; k < arr.length; k++) {
+                    for (let j = 0; j < englishAlphabetLowerCase.length; j++) {
+                        if (arr[k] == englishAlphabetLowerCase[j]) {
+                            if (englishAlphabetLowerCase[j + 13] == undefined) {
+                                sum = j + 13;
+                                secondArr.push(englishAlphabetLowerCase[sum - englishAlphabetLowerCase.length]);
+                            } else {
+                                secondArr.push(englishAlphabetLowerCase[j + 13]);
+                            }
+                        } 
+                    }
                 }
             }
         }
